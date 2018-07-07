0fe0cc5c76e3123d9b218f7c82e31d736e3735f1
QianGuoqing <qian_guoqing@yeah.net>
2018-07-07 16:15:03 +0800

    初始化项目

---
+++ im-app/src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    <mt-button type="default">default</mt-button>
+    <mt-button type="primary">primary</mt-button>
+    <mt-button type="danger">danger</mt-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="stylus" scoped>
+  .hello
+    margin 100px
+</style>
